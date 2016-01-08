554726614ff3f0a2f618be3bb3b2a44ce1619b4e
themarioga <marioga2512@gmail.com>
2016-01-08 18:09:44 +0100

    Esta alcanzando masa critica

--- resources/functions.js
+++ resources/functions.js
@@ -1,7 +1,7 @@
 var energyCong = 0, energyFrig = 0;
 var objfrigo, objconge;
-var prevTempFrigo, prevTempConge;
-var tempfrigo = 2, tempconge = -20;
+var prevTempFrigo, prevTempConge, prevTermo;
+var tempfrigo = 4, tempconge = -18;
 var puertafrig = false, puertacong = false, frigoon = false, congeon = false, ecoon = false;
 function toggleFrigo(){
 	if (frigoon) {
@@ -41,9 +41,22 @@ function toggleEco(){
 		ecoon = true;
 		$("#eco-btn .b-large").css("color", "green");
 		$("#eco-btn .infotext").html("ECO ON");
-		changeDefaultTempFrigo(4);
-		changeDefaultTempConge(-18);
+		changeDefaultTempFrigo(8);
+		changeDefaultTempConge(-16);
 		toast("Activado modo ECO", 2000);
+	}
+}
+function toogleIco(ico){
+	var tipo = $("#ico_"+ico).html();
+	switch (ico){
+		case 'wifi':
+			if (tipo == "wifi")	$("#ico_"+ico).html("signal_wifi_off");
+			else $("#ico_"+ico).html("wifi");
+		break;
+		case 'volume':
+			if (tipo == "volume_up") $("#ico_"+ico).html("volume_off");
+			else $("#ico_"+ico).html("volume_up");
+		break;
 	}
 }
 function changeDefaultTempFrigo(temp){
@@ -89,6 +102,29 @@ function changeCongEnergy(temp){
 	energyCong = temp;
 	changeTotalEnergy();
 }
+function changeExtTemp(temp){
+	$("#ext_temp").html(temp);
+}
+function changeFrigTemp(temp){
+	$("#frig_temp").html(Math.round(temp));
+	if (temp > tempfrigo && !puertafrig && frigoon) electro.motorFrigorifico(true);
+	else if (temp < tempfrigo) electro.motorFrigorifico(false);
+}
+function changeCongTemp(temp){
+	$("#cong_temp").html(Math.round(temp));
+	if (temp > tempconge && !puertacong && congeon) electro.motorCongelador(true);
+	else if (temp < tempconge)  electro.motorCongelador(false);
+}
+function frigo_bubble(){
+	bubble("frigotermo");
+	$("#congetermo-range").hide();
+	$("#frigotermo-range").show();
+}
+function conge_bubble(){
+	bubble("congetermo");
+	$("#frigotermo-range").hide();
+	$("#congetermo-range").show();
+}
 function digiDateTime(date){
 	digiDate(date);
 	digiClock(date);
@@ -116,35 +152,42 @@ function digiClock(crTime) {
 function toast(msg, time, obj){
 	if (typeof obj == "undefined") {
 		obj = $('<div class="toast">'+msg+'</div>');
-		$("body").prepend(obj);
+		$("#toast-container").prepend(obj);
 	} else obj.html(msg);
 	obj.show("fast");
 	if (time > 0) setTimeout(function() {obj.hide("fast"); obj.remove();}, time);
 	else return obj;
 }
-function toogleIco(ico){
-	var tipo = $("#ico_"+ico).html();
-	switch (ico){
-		case 'wifi':
-			if (tipo == "wifi")	$("#ico_"+ico).html("signal_wifi_off");
-			else $("#ico_"+ico).html("wifi");
-		break;
-		case 'volume':
-			if (tipo == "volume_up") $("#ico_"+ico).html("volume_off");
-			else $("#ico_"+ico).html("volume_up");
-		break;
+function bubble(id){
+	var correccion = 0;
+	if ($(window).width() > $(window).height()) $("#bubble").css("bottom", "15.5vh");
+	else {
+		$("#bubble").css("bottom", "13vh");
+		correccion = -30;
 	}
+	var posleft = $("#"+id+"-btn").offset().left-$("#"+id+"-btn").width()/2+correccion;
+	$("#bubble").css("left", posleft);
+	if ($("#bubble").css("display") == "none") $("#bubble").show();
+	else if (id == prevTermo) {
+		$("#bubble").hide();
+		id = "";
+	}
+	prevTermo = id;
 }
-function changeExtTemp(temp){
-	$("#ext_temp").html(temp);
-}
-function changeFrigTemp(temp){
-	$("#frig_temp").html(Math.round(temp));
-	if (temp > tempfrigo && !puertafrig && frigoon) electro.motorFrigorifico(true);
-	else if (temp < tempfrigo) electro.motorFrigorifico(false);
-}
-function changeCongTemp(temp){
-	$("#cong_temp").html(Math.round(temp));
-	if (temp > tempconge && !puertacong && congeon) electro.motorCongelador(true);
-	else if (temp < tempconge)  electro.motorCongelador(false);
+function range(id, start, min, max){
+	var slider = document.getElementById(id+'-range');
+	noUiSlider.create(slider, {
+		start: [ start ],
+		step: 1, // Slider moves in increments of '10'
+		direction: 'rtl', // Put '0' at the bottom of the slider
+		orientation: 'vertical', // Orient the slider vertically
+		range: { // Slider can select '0' to '100'
+			'min': min,
+			'max': max
+		},
+		pips: { // Show a scale with the slider
+			mode: 'steps',
+			density: 2
+		}
+	});
 }
